cad12b268e492daf50e5ab74953c1c18e4e08d9d
martin mithi <gathitumithi9000@gmail.com>
2023-04-24 14:26:36 +0300

    Set server proxy to localhost:3000

--- src/constant.js
+++ src/constant.js
@@ -1,1 +1,2 @@
-export const url = "https://touted-tub-production.up.railway.app";
+// export const url = "https://touted-tub-production.up.railway.app";
+export const url = "http://localhost:3000";
